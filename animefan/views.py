36ba0bf5e960d8059bda60c79b97b0d5396fba1b
Asad <asad.azam.asad@gmail.com>
2020-10-14 19:40:18 +0530

    idk?

--- animefan/views.py
+++ animefan/views.py
@@ -7,7 +7,6 @@ from .models import Anime
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
-    # return HttpResponse("Hello, world. You're at the animefan index.")
 
 def detailanimeid(request, anime_id):
     anime = Anime.objects.get(anime_id = anime_id)
