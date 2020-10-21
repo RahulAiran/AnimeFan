b85a7fe46c248d9781d7c1ab05027dcaf3f49b5e
Asad <asad.azam.asad@gmail.com>
2020-10-21 21:25:16 +0530

    Stated Django Support

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -119,9 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-STATICFILES_DIRS = [
-     "/assets/",
- ]
+STATIC_URL = '/static/'
