2c1b658a3509cc562dbd6ea646db2091bf865993
Asad <asad.azam.asad@gmail.com>
2020-10-25 11:31:16 +0530

    Added Mood

--- animefan/urls.py
+++ animefan/urls.py
@@ -2,7 +2,8 @@ from animefan.views import search
 from django.urls import path, register_converter
 
 from . import converters, views
-from .views import search
+from django.conf.urls import url
+from .views import mood, search, popularity, rating
 
 register_converter(converters.FloatUrlParameterConverter, 'float')
 
@@ -11,6 +12,12 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('tribute/', views.tribute, name='tribute'),
     path('search/',  search.as_view(), name='search'),
+    url(r'^$', search.as_view(), name='search'),
+    path('filter/popularity-<int:number>/',  popularity.as_view(), name='popularity'),
+    path('filter/rating-<int:number>/', rating.as_view(), name='rating'),
+
+    path('filter/mood-<str:mood>-<int:number>/', mood.as_view(), name='mood'),
+
     path('<int:anime_id>/', views.detailanimeid, name='JSON'),
     path('name-<str:name>/', views.detailname, name='JSON'),
     path('mood-<str:mood>-<int:number>/', views.detailmood, name='JSON'),
--- animefan/views.py
+++ animefan/views.py
@@ -1,9 +1,12 @@
 # from django.http import request
+from pdb import set_trace
+from django.http import response
 from django.shortcuts import render
 # from django.http import HttpResponse
-import requests
+# import requests
 from django.http import JsonResponse
 from django.views.generic import View
+from requests.api import options, request
 
 from .models import Anime
 
@@ -22,116 +25,199 @@ class search(View):
     template_name = "search.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, {'hidden': True})
 
     def post(self, request):
+        # import pdb; pdb.set_trace()
         query = self.request.POST.get('searchbar', None)
-        # response = detailname(request, query)
-        response = requests.get('http://127.0.0.1:8000/animefan/name-'+query).json()
-        return render(request, self.template_name, {'response': response})
+        response = detailname(request, query, 'WEB')
+        return render(request, self.template_name, {'response': response, 'hidden': False})
 
-def detailanimeid(request, anime_id):
+class popularity(View):
+    template_name = "search.html"
+
+    def get(self, request, number):
+        # import pdb; pdb.set_trace()
+        response = detailpopularity(request, number, 'WEB')
+        return render(request, self.template_name, {'response': response, 'hidden': False})
+
+class rating(View):
+    template_name = "search.html"
+
+    def get(self, request, number):
+        response = toprated(request, number, 'WEB')
+        return render(request, self.template_name, {'response': response, 'hidden': False})
+
+class mood(View):
+    template_name = "search.html"
+
+    def get(self, request, mood, number):
+        response = detailmood(request, mood, number, 'WEB')
+        return render(request, self.template_name, {'response': response, 'hidden': False})
+
+def detailanimeid(request, anime_id, options='NULL'):
     i = 0
-    anime = Anime.objects.get(anime_id = anime_id)
-
-    response = [{}]
-
-    response[i]["SNo"] = i+1
-    response[i]["AnimeID"] = anime.anime_id
-    response[i]["AnimeName"] = anime.name
-    response[i]["Genre"] = anime.genre
-    response[i]["AnimeType"] = anime.animetype
-    response[i]["Episodes"] = anime.episodes
-    response[i]["Rating"] = anime.rating
-    response[i]["Members"] = anime.members
-    response[i]["Mood"] = anime.mood
-
-    return JsonResponse(response, safe=False)
+    anime = Anime.objects.get(anime_id = int(anime_id))
+    response = {
+        'SNo' : i+1,
+        'AnimeID' : anime.anime_id,
+        'AnimeName' :  anime.name,
+        'Genre' : anime.genre, 
+        'AnimeType' : anime.animetype,
+        'Episodes' : anime.episodes,
+        'Rating' : anime.rating,
+        'Members' : anime.members,
+        'Mood': anime.mood
+    }
+    # return response
+    if options == 'NULL':
+        return JsonResponse(response, safe=False)
+    else:
+        return response
     
     # return HttpResponse("You're looking at mood : %s" %anime.mood ) 
 
-def detailname(request, name):
+def detailname(request, name, options ='NULL'):
     i = 0
+    result = []
     name = name.title()
-    anime = Anime.objects.filter(name__contains = name)
+    animes = Anime.objects.filter(name__contains = name)
 
-    response = [{} for x in range(len(anime))]
+    # response = [{} for x in range(len(anime))]
 
-    while i < len(anime):
-        response[i]["SNo"] = i+1
-        response[i]["AnimeID"] = anime[i].anime_id
-        response[i]["AnimeName"] = anime[i].name
-        response[i]["Genre"] = anime[i].genre
-        response[i]["AnimeType"] = anime[i].animetype
-        response[i]["Episodes"] = anime[i].episodes
-        response[i]["Rating"] = anime[i].rating
-        response[i]["Members"] = anime[i].members
-        response[i]["Mood"] = anime[i].mood
-        i += 1
+    # while i < len(anime):
+    for anime in animes:
+        response = {
+            'SNo' : i+1,
+            'AnimeID' : anime.anime_id,
+            'AnimeName' :  anime.name,
+            'Genre' : anime.genre, 
+            'AnimeType' : anime.animetype,
+            'Episodes' : anime.episodes,
+            'Rating' : anime.rating,
+            'Members' : anime.members,
+            'Mood': anime.mood
+        }
+        i+=1
+        result.append(response)
+    
+    # return result
+    if options == 'NULL':
+        return JsonResponse(result, safe=False)
+    else:
+        return result
 
-    return JsonResponse(response, safe=False)
-
-def detailmood(request, mood, number):
+def detailmood(request, mood, number, options='NULL'):
     i = 0
+    result = []
+    number = (number-1) * 50
     mood = mood.title()
-    anime = Anime.objects.filter(mood = mood)
-    response = [{} for x in range(number)]
-    while i < number :
-        response[i]["SNo"] = i+1
-        response[i]["AnimeID"] = anime[i].anime_id
-        response[i]["AnimeName"] = anime[i].name
-        response[i]["Genre"] = anime[i].genre
-        response[i]["AnimeType"] = anime[i].animetype
-        response[i]["Episodes"] = anime[i].episodes
-        response[i]["Rating"] = anime[i].rating
-        response[i]["Members"] =  anime[i].members
-        response[i]["Mood"] = anime[i].mood
-        i += 1 
+    animes = Anime.objects.filter(mood = mood)[number:number+50]
+    
+    for anime in animes:
+        response = {
+            'SNo' : i+1,
+            'AnimeID' : anime.anime_id,
+            'AnimeName' :  anime.name,
+            'Genre' : anime.genre, 
+            'AnimeType' : anime.animetype,
+            'Episodes' : anime.episodes,
+            'Rating' : anime.rating,
+            'Members' : anime.members,
+            'Mood': anime.mood
+        }
+        i+=1
+        result.append(response) 
 
-    return JsonResponse(response, safe=False)
+    if options == 'NULL':
+        return JsonResponse(result, safe=False)
+    else:
+        return result
 
-def detailrating(request, rating):
+def detailrating(request, rating, number, options='NULL'):
     i = 0
-    anime = Anime.objects.filter(rating__gte = rating)
-    response = [{} for x in range(len(anime))]
-    while i < len(anime) :
-        response[i]["SNo"] = i+1
-        response[i]["AnimeID"] = anime[i].anime_id
-        response[i]["AnimeName"] = anime[i].name
-        response[i]["Genre"] = anime[i].genre
-        response[i]["AnimeType"] = anime[i].animetype
-        response[i]["Episodes"] = anime[i].episodes
-        response[i]["Rating"] = anime[i].rating
-        response[i]["Members"] =  anime[i].members
-        response[i]["Mood"] = anime[i].mood
-        i += 1 
+    result = []
+    number = (number-1) * 50
+    animes = Anime.objects.filter(rating__gte = rating)[number:number+50]
+    
+    for anime in animes:
+        response = {
+            'SNo' : i+1,
+            'AnimeID' : anime.anime_id,
+            'AnimeName' :  anime.name,
+            'Genre' : anime.genre, 
+            'AnimeType' : anime.animetype,
+            'Episodes' : anime.episodes,
+            'Rating' : anime.rating,
+            'Members' : anime.members,
+            'Mood': anime.mood
+        }
+        i+=1
+        result.append(response) 
 
-    return JsonResponse(response, safe=False)
+    if options == 'NULL':
+        return JsonResponse(result, safe=False)
+    else:
+        return result
 
-def detailpopularity(request, number):
+def toprated(request, number, options='NULL'):
     i = 0
-    number = number * 50
-    anime = Anime.objects.order_by('-members')[number:number+50]
-    response = [{} for x in range(len(anime))]
-    while i < len(anime) :
-        response[i]["SNo"] = i+1
-        response[i]["AnimeID"] = anime[i].anime_id
-        response[i]["AnimeName"] = anime[i].name
-        response[i]["Genre"] = anime[i].genre
-        response[i]["AnimeType"] = anime[i].animetype
-        response[i]["Episodes"] = anime[i].episodes
-        response[i]["Rating"] = anime[i].rating
-        response[i]["Members"] =  anime[i].members
-        response[i]["Mood"] = anime[i].mood
-        i += 1
+    result = []
+    number = (number-1) * 50
+    animes = Anime.objects.order_by('-rating')[number:number+50]
+    
+    for anime in animes:
+        response = {
+            'SNo' : i+1,
+            'AnimeID' : anime.anime_id,
+            'AnimeName' :  anime.name,
+            'Genre' : anime.genre, 
+            'AnimeType' : anime.animetype,
+            'Episodes' : anime.episodes,
+            'Rating' : anime.rating,
+            'Members' : anime.members,
+            'Mood': anime.mood
+        }
+        i+=1
+        result.append(response) 
 
-    return JsonResponse(response, safe=False)
+    if options == 'NULL':
+        return JsonResponse(result, safe=False)
+    else:
+        return result
 
-def detailgenre(request, genre, number):
+def detailpopularity(request, number, options='NULL'):
+    i = 0
+    result = []
+    number = (number-1) * 50
+    animes = Anime.objects.order_by('-members')[number:number+50]
+    
+    for anime in animes:
+        response = {
+            'SNo' : i+1,
+            'AnimeID' : anime.anime_id,
+            'AnimeName' :  anime.name,
+            'Genre' : anime.genre, 
+            'AnimeType' : anime.animetype,
+            'Episodes' : anime.episodes,
+            'Rating' : anime.rating,
+            'Members' : anime.members,
+            'Mood': anime.mood
+        }
+        i+=1
+        result.append(response) 
+
+    if options == 'NULL':
+        return JsonResponse(result, safe=False)
+    else:
+        return result
+
+def detailgenre(request, genre, number, options='NULL'):
     i = 0
     genre = genre.title()
     anime = Anime.objects.filter(genre = genre)
     response = [{} for x in range(number)]
+    
     while i < number :
         response[i]["SNo"] = i+1
         response[i]["AnimeID"] = anime[i].anime_id
@@ -142,6 +228,9 @@ def detailgenre(request, genre, number):
         response[i]["Rating"] = anime[i].rating
         response[i]["Members"] =  anime[i].members
         response[i]["Mood"] = anime[i].mood
-        i += 1 
+        i += 1
 
-    return JsonResponse(response, safe=False)
+    if options == 'NULL':
+        return JsonResponse(response, safe=False)
+    else:
+        return render(request, "search.html", {'response': response, 'hidden': False})
